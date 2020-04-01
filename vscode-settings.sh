c30a85c1010d458210fdc0abc4d9bccee1ca18d0
nmstreethran <nmstreethran@gmail.com>
2020-04-02 01:09:30 +0200

    update vscode settings

--- vscode-settings.sh
+++ vscode-settings.sh
@@ -2,6 +2,6 @@
 
 # copy vscode settings
 
-cp "$HOME/.config/Code - Insiders/User/settings.json" vscode-settings/settings.json
-cp "$HOME/.config/Code - Insiders/User/keybindings.json" vscode-settings/keybindings.json
-ls -1d $HOME/.vscode-insiders/extensions/*/ | awk -F "/" "{print \$(NF-1)}" > vscode-settings/extensions.txt
+cp $HOME/.config/VSCodium/User/settings.json vscode-settings/settings.json
+cp $HOME/.config/VSCodium/User/keybindings.json vscode-settings/keybindings.json
+ls -1d $HOME/.vscode-oss/extensions/*/ | awk -F "/" "{print \$(NF-1)}" > vscode-settings/extensions.txt
