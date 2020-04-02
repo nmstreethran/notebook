3f1f25f8c0a23243b7041d792a3a85c41a575e1c
nmstreethran <nmstreethran@gmail.com>
2020-04-02 07:03:43 +0200

    change settings extension to jsonc

--- vscode-settings.sh
+++ vscode-settings.sh
@@ -2,6 +2,6 @@
 
 # copy vscode settings
 
-cp $HOME/.config/VSCodium/User/settings.json vscode-settings/settings.json
-cp $HOME/.config/VSCodium/User/keybindings.json vscode-settings/keybindings.json
+cp $HOME/.config/VSCodium/User/settings.json vscode-settings/settings.jsonc
+cp $HOME/.config/VSCodium/User/keybindings.json vscode-settings/keybindings.jsonc
 ls -1d $HOME/.vscode-oss/extensions/*/ | awk -F "/" "{print \$(NF-1)}" > vscode-settings/extensions.txt
