105a6443819ccfc9a07ade1bb6ae5551cbfe8da2
Nithiya Streethran <nmstreethran@gmail.com>
2022-04-28 01:44:40 +0100

    update settings

---
+++ .windows/README.md
@@ -0,0 +1,18 @@
+# Windows
+
+List of packages installed through `winget` (`.windows/pkgs-winget.json`):
+
+```powershell
+winget export --output .\.windows\pkgs-winget.json
+# or
+winget export -o .\.windows\pkgs-winget.json
+```
+
+Skyrim SE plugin list (`.windows\skyrimse-plugins.txt`) and load order (`.windows/skyrimse-loadorder.txt`) for mods managed with Vortex:
+
+```powershell
+cp $HOME\AppData\Roaming\Vortex\skyrimse\profiles\yGZRItibI\plugins.txt .\.windows\skyrimse-plugins.txt
+cp $HOME\AppData\Roaming\Vortex\skyrimse\profiles\yGZRItibI\loadorder.txt .\.windows\skyrimse-loadorder.txt
+```
+
+Windows Terminal settings are saved in `.windows/term-settings.json`.
--- settingsbackup.sh
+++ settingsbackup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # copy VSCode settings
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -11,13 +11,13 @@ fi
 cp *.md gh-wiki
 mv gh-wiki/README.md gh-wiki/Home.md
 
-# copy other settings
-cp ~/.zshrc .manjaro/.zshrc
-cp ~/.ssh/config .manjaro/ssh-config.txt
-cp /etc/fonts/local.conf .manjaro/fonts-local.xml
-cp ~/.local/share/konsole/*.profile .manjaro/konsole.conf
-cp ~/.kde4/share/config/kdeglobals .manjaro/kdeglobals.conf
-cp ~/.local/share/kxmlgui5/okular/part.rc .manjaro/okular-part.xml
+# # copy other settings
+# cp ~/.zshrc .manjaro/.zshrc
+# cp ~/.ssh/config .manjaro/ssh-config.txt
+# cp /etc/fonts/local.conf .manjaro/fonts-local.xml
+# cp ~/.local/share/konsole/*.profile .manjaro/konsole.conf
+# cp ~/.kde4/share/config/kdeglobals .manjaro/kdeglobals.conf
+# cp ~/.local/share/kxmlgui5/okular/part.rc .manjaro/okular-part.xml
 
 # # list of packages
 # pamac list --explicitly-installed --quiet > .manjaro/pkgs.txt
