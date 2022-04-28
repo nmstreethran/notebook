d33b7358014f34716e80c5ead3d6841eeaa7d7ab
Nithiya Streethran <nmstreethran@gmail.com>
2022-04-28 13:41:40 +0100

    reorganise

--- settingsbackup.sh
+++ settingsbackup.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 # copy VSCode settings
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    cp ~/.config/VSCodium/User/settings.json .vscode/settings.jsonc
-    cp ~/.config/VSCodium/User/keybindings.json .vscode/keybindings.jsonc
-    for dir in $(ls ~/.vscode-oss/extensions/);
-    do echo ${dir%-*};
-    done > .vscode/extensions.txt
-fi
-
-# copy all notes into wiki
-cp *.md gh-wiki
-mv gh-wiki/README.md gh-wiki/Home.md
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+cp ~/.config/VSCodium/User/settings.json .vscode/settings.jsonc
+cp ~/.config/VSCodium/User/keybindings.json .vscode/keybindings.jsonc
+for dir in $(ls ~/.vscode-oss/extensions/);
+do echo ${dir%-*};
+done > .vscode/extensions.txt
 
 # copy other settings
 cp ~/.zshrc .manjaro/.zshrc
@@ -24,3 +19,8 @@ cp ~/.local/share/kxmlgui5/okular/part.rc .manjaro/okular-part.xml
 # list of packages
 # pamac list --explicitly-installed --quiet > .manjaro/pkgs.txt
 pamac list --foreign --quiet > .manjaro/pkgs-aur.txt
+# fi
+
+# copy all notes into wiki
+cp notes/*.md gh-wiki
+cp README.md gh-wiki/Home.md
