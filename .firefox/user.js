cb6d523f369992a9e06754d29f36dae89fbb49e2
Nithiya Streethran <nmstreethran@gmail.com>
2022-09-04 23:23:47 +0100

    update

--- .firefox/user.js
+++ .firefox/user.js
@@ -2,6 +2,6 @@
 
 user_pref("browser.search.hiddenOneOffs", "Amazon.co.uk,Bing,eBay");
 user_pref("browser.toolbars.bookmarks.visibility", "never");
-user_pref("font.name.monospace.x-western", "Cascadia Code");
+user_pref("font.name.monospace.x-western", "Cascadia Mono");
 user_pref("font.name.sans-serif.x-western", "Source Sans 3");
 user_pref("view_source.wrap_long_lines", true);
