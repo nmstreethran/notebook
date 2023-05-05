b522c186c9a06755e1fabdebdf3cdf236b4208ad
Nithiya Streethran <nmstreethran@gmail.com>
2023-05-05 12:02:37 +0100

    update settings

--- .zotero/user.js
+++ .zotero/user.js
@@ -1,15 +1,24 @@
 /* https://kb.mozillazine.org/User.js_file */
 
 user_pref("extensions.zotero.automaticSnapshots", false);
+
 user_pref("extensions.zotero.dataDir", "/home/nms/Zotero");
-user_pref("extensions.zotero.export.quickCopy.setting", "bibliography=https://raw.githubusercontent.com/nmstreethran/csl-custom-styles/main/the-open-university-harvard-modified.csl");
+
+user_pref("extensions.zotero.export.quickCopy.setting", "bibliography=https://raw.githubusercontent.com/nmstreethran/csl-custom-styles/main/harvard-cite-them-right-11th-edition-modified.csl");
 user_pref("extensions.zotero.export.translatorSettings", "{\"exportNotes\":false,\"exportFileData\":false,\"useJournalAbbreviation\":false,\"keepUpdated\":true,\"worker\":true,\"includeAnnotations\":false}");
+
 user_pref("extensions.zotero.fileHandler.pdf", "/bin/okular");
+
+user_pref("extensions.zotero.newItemTypeMRU", "book,bookSection,webPage,journalArticle,document");
+
+user_pref("extensions.zotero.translators.better-bibtex.DOIandURL", "doi");
 user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "authEtal2.lower + year.prefix(_)");
+user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "authEtal2.lower + year.prefix(_)");
 user_pref("extensions.zotero.translators.better-bibtex.exportTitleCase", false);
 user_pref("extensions.zotero.translators.better-bibtex.importSentenceCase", "off");
-user_pref("extensions.zotero.translators.better-bibtex.skipFields", "abstract,shorttitle,keywords,file,annotation");
+user_pref("extensions.zotero.translators.better-bibtex.skipFields", "abstract,shorttitle,keywords,file,annotation,shortjournal");
 user_pref("extensions.zotero.translators.better-bibtex.warnBulkModify", 0);
+
 user_pref("extensions.zotero.useDataDir", true);
 
 user_pref("extensions.zoteroOpenOfficeIntegration.skipInstallation", true);
@@ -23,3 +32,5 @@ user_pref("extensions.zotfile.removeDiacritics", true);
 user_pref("extensions.zotfile.replace_blanks", true);
 user_pref("extensions.zotfile.source_dir", "/run/media/nms/Backup/Documents/Zotero");
 user_pref("extensions.zotfile.source_dir_ff", false);
+user_pref("extensions.zotfile.truncate_title", false);
+user_pref("extensions.zotfile.truncate_title_max", false);
