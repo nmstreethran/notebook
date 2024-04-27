f71073e8bd535eda0457f97f491142b6591f73ba
Nithiya Streethran <nmstreethran@gmail.com>
2024-04-27 15:08:38 +0100

    update configs

--- linux/firefox-user.js
+++ linux/firefox-user.js
@@ -1,8 +1,7 @@
-/* https://kb.mozillazine.org/User.js_file */
-
-user_pref("browser.download.skipConfirmLaunchExecutable", true);
 user_pref("browser.search.hiddenOneOffs", "Amazon.co.uk,Bing,eBay");
 user_pref("browser.toolbars.bookmarks.visibility", "never");
-user_pref("font.name.monospace.x-western", "Cascadia Mono");
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.monospace.x-western", "Fira Mono");
 user_pref("font.name.sans-serif.x-western", "Source Sans 3");
+user_pref("font.name.serif.x-western", "Roboto Slab");
 user_pref("view_source.wrap_long_lines", true);
--- settingsbackup.sh
+++ settingsbackup.sh
@@ -9,36 +9,40 @@ mkdir -p linux
 # done > linux/vscode-extensions.txt
 cat ~/.vscode-oss/extensions/extensions.json | \
 jq --raw-output '.[] | .identifier.id' | sort > linux/vscode-extensions.txt
+cp ~/.config/VSCodium/User/settings.json linux/vscode-settings.json
+cp ~/.config/VSCodium/User/keybindings.json linux/vscode-keybindings.jsonc
 
-cp --parents ~/.local/share/konsole/*.profile linux
-for setting in .config/VSCodium/User/settings.json .config/VSCodium/User/keybindings.json .zshrc .local/share/kxmlgui5/okular/part.rc .config/okularpartrc .config/spectaclerc .config/kglobalshortcutsrc .xscreensaver .ssh/config;
+# cp --parents ~/.local/share/konsole/*.profile linux
+for setting in .zshrc .profile .bashrc .config/yakuakerc .config/okularpartrc .config/kglobalshortcutsrc;
 do cp --parents ~/${setting} linux
 done
-sed --in-place "/lastSaveAsLocation=/d" linux/home/nms/.config/spectaclerc
-sed --in-place "/lastSaveLocation=/d" linux/home/nms/.config/spectaclerc
-sed --in-place "/lastImageSaveLocation=/d" linux/home/nms/.config/spectaclerc
-sed --in-place "/Recent URLs/d" linux/home/nms/.config/spectaclerc
-sed --in-place "/Recent Files/d" linux/home/nms/.config/spectaclerc
+# sed --in-place "/lastSaveAsLocation=/d" linux/home/nms/.config/spectaclerc
+# sed --in-place "/lastSaveLocation=/d" linux/home/nms/.config/spectaclerc
+# sed --in-place "/lastImageSaveLocation=/d" linux/home/nms/.config/spectaclerc
+# sed --in-place "/Recent URLs/d" linux/home/nms/.config/spectaclerc
+# sed --in-place "/Recent Files/d" linux/home/nms/.config/spectaclerc
 # sed --in-place "/window-position=/d" linux/home/nms/.config/spectaclerc
 # sed --in-place "/SplitterSizes=/d" linux/home/nms/.config/spectaclerc
 # sed --in-place "/ViewMode=/d" linux/home/nms/.config/spectaclerc
 
 # list of packages
-pacman -Qqen > linux/pkgs-pacman.txt
-pacman -Qqem > linux/pkgs-aur.txt
+# pacman -Qqen > linux/pkgs-pacman.txt
+# pacman -Qqem > linux/pkgs-aur.txt
 
 # Firefox
 # ls -1 ~/.mozilla/firefox/*.dev-edition-default/extensions/ |
 # awk -e '/^[A-Za-z]/ { print }' > linux/firefox-extensions
-jq '
+for edition in dev-edition-default default-release;
+do jq '
     [.addons[] |
     if (.id | test("^(?!.*mozilla).*$"))
     then ({ id: .id, name: .defaultLocale.name })
     else empty
     end] | sort_by(.name)
-' ~/.mozilla/firefox/*.dev-edition-default/extensions.json > \
-linux/firefox-extensions.json
-cp --parents ~/.mozilla/firefox/*.dev-edition-default/user.js linux
+' ~/.mozilla/firefox/*.${edition}/extensions.json > \
+linux/firefox-${edition}-extensions.json
+done
+cp ~/.mozilla/firefox/*.dev-edition-default/user.js linux/firefox-user.js
 
 # Zotero
 # ls -1 ~/.zotero/zotero/*/extensions/ > linux/zotero-extensions.txt
@@ -49,7 +53,7 @@ jq '
     else empty
     end] | sort_by(.name)
 ' ~/.zotero/zotero/*/extensions.json > linux/zotero-extensions.json
-cp --parents ~/.zotero/zotero/*/user.js linux
+cp ~/.zotero/zotero/*/user.js linux/zotero-user.js
 # fi
 
 # symlink all notes into wiki
